e91b98d2dde5b8bb2d0ecfba47b4ab8326a6d143
Naveen jalaraddi <naveenjalaraddi82@gmail.com>
2025-12-06 23:12:59 +0530

    Add files via upload

---
+++ PatternPrinting.java
@@ -0,0 +1,24 @@
+/*pattern printing pattern like this
+ *   
+ *   * * * * *
+ *   * * * *
+ *   * * *
+ *   * * 
+ *   *
+ *   
+ */
+
+public class PatternPrinting {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<=5; i++) {
+			for(int j=0; j<=5-i; j++) {
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
