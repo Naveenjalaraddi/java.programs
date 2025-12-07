aa90937dca3d4dda160f9a6ca2496c679c23f75c
Naveen jalaraddi <naveenjalaraddi82@gmail.com>
2025-12-07 20:57:57 +0530

    Add files via upload

---
+++ ArrayEx.java
@@ -0,0 +1,30 @@
+
+//Example of array topic 
+import java.util.Scanner;
+
+public class ArrayEx {
+
+	public static void main(String[] args) {
+
+		// Declare an array initialize with user input print the array elements
+		Scanner scn = new Scanner(System.in);
+		int no;
+
+		System.out.print("Enter your required array elements: ");
+		no = scn.nextInt();
+
+		int[] arra = new int[no];
+
+		System.out.println("Enter array elements one by one: ");
+		for (int i = 0; i < no; i++) {
+
+			arra[i] = scn.nextInt();
+
+		}
+		for (int j = 0; j < no; j++) {
+			System.out.println(+j + ".element: " + arra[j]);
+
+		}
+	}
+
+}
