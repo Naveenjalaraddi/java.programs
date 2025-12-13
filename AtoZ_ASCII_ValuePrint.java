311db72ce3e4e9f852b57cafca0446db9dc2a0fb
Naveen jalaraddi <naveenjalaraddi82@gmail.com>
2025-12-13 21:23:03 +0530

    Add files via upload

---
+++ AtoZ_ASCII_ValuePrint.java
@@ -0,0 +1,25 @@
+
+public class AtoZ_ASCII_ValuePrint {
+
+	public static void main(String[] args) {
+		
+		for(char i='A'; i<='Z'; i++) {
+			System.out.print(i+"  ");
+		}
+		System.out.println();
+		for(int i='A'; i<='Z'; i++) {
+			System.out.print(i+" ");
+		}
+		System.out.println();
+		System.out.println("===============================================================================");
+		for(char i='a'; i<='z'; i++) {
+			System.out.print(i+"  ");
+		}
+		System.out.println();
+		for(int i='a';i<='z'; i++) {
+			System.out.print(i+" ");
+		}
+		
+	}
+
+}
