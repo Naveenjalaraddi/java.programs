b7cc5f2fa3c1e1a91641d6c49f9d770e3a5a3ae4
Naveen jalaraddi <naveenjalaraddi82@gmail.com>
2025-12-21 22:56:20 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,34 @@
+
+/*   Assignment-3. 
+
+        ● Create a class Person 
+
+        ● Add member variables name as String, age and salary as int 
+
+        ● Initialize the member variable along with declaration. 
+
+        ● Now put the previous Person class in a package com.anudip.learning 
+
+        ● Add amain method. Add a print message “Test Successful”. 
+
+        ● Run the class after compilation. 
+
+        ● Modify the class paths to see the error messages on the console. 
+
+*/
+
+package anudip.assignments;
+
+public class Person {
+	// Member variables initialized at declaration
+	String name = "Naveen";
+	int age = 25;
+	int salary = 30000;
+
+	// Main method
+	public static void main(String[] args) {
+		System.out.println("Test Successful");
+
+	}
+
+}
