274dae4c2e2d4934b9966ef53fc1e998ac974d80
Naveen jalaraddi <naveenjalaraddi82@gmail.com>
2025-12-22 22:40:55 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+package com.anudip.learning;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        
+        animal.makeSound();
+        dog.makeSound();
+        cat.makeSound();
+
+	}
+
+}
